20f31d9a8356edbbef8cecda3a04cdfe365f3f78
AnhHius <hay55881@gmail.com>
2025-12-18 08:30:27 +0700

    Main.java

--- FlastCard/Main.java
+++ FlastCard/Main.java
@@ -1,11 +1,26 @@
-import javax.swing.*;
+import Model.FlashCardManager;
 import Model.UserManager;
+import View.FlashCardAppView;
+import Model.FlashCard;
+import Control.CardControl;
+import View.LoginView;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+import Model.UserManager;
+import View.LoginView;
+import javax.swing.SwingUtilities;
 
 public class Main {
     public static void main(String[] args) {
+
         SwingUtilities.invokeLater(() -> {
             UserManager userManager = new UserManager();
-            new View.LoginView(userManager); // Chỉ mở login
+            // Mở màn hình login
+            new LoginView(userManager);
         });
     }
 }
